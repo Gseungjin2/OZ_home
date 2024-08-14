dfd08d916f6f5d23aa3937e7f98d0c6e4d75c2d2
Gseungjin2 <jchsekfr5@gmail.com>
2024-08-15 02:21:42 +0900

    1

---
+++ test.py
@@ -0,0 +1,8 @@
+def sum_int(start, end,):
+    total = 0
+    for i in range(start, end + 1):  
+        total += i
+    print("%d~%d 정수 합계 : %d" % (start, end, total))
+
+sum_int(30, 50)
+sum_int(500, 600)
