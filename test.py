2999dd11b8e8fdc93dae3cb22fc6869a18adf6b3
Gseungjin2 <jchsekfr5@gmail.com>
2024-08-16 00:40:01 +0900

    1

--- test.py
+++ test.py
@@ -1,8 +1,35 @@
-def sum_int(start, end,):
-    total = 0
-    for i in range(start, end + 1):  
-        total += i
-    print("%d~%d 정수 합계 : %d" % (start, end, total))
+# def sum_int(start, end,):
+#     total = 0
+#     for i in range(start, end + 1):  
+#         total += i
+#     print("%d~%d 정수 합계 : %d" % (start, end, total))
 
-sum_int(30, 50)
-sum_int(500, 600)
+# sum_int(30, 50)
+# sum_int(500, 600)
+
+
+# # 최소/최대 값 구분코드
+# def solution(s):
+#     # [1, 2, 3, 4] 숫자형
+#     s = list(map(int,s.split(" ")))
+#     result = f'{min(s)} {max(s)}'
+#     return result
+    
+# result1 = solution('1 2 3 4')
+# print(solution(result1))
+
+
+# def solution(s):
+#     answer = sorted([int(x) for x in s.split()])
+#     return str(answer[0]) + " " + str(answer[-1])
+
+# result = solution('1 2 3 4')
+# print(solution(result))
+
+def solution(A,B):
+    a = list(map(int,A))
+    b = list(map(int,B))
+    
+    return result
+
+result = solution()
